af3ea7d25f93018178199ef0d0f5db185e3c16f5
phbomfim <paulobommfim@gmail.com>
2022-06-27 21:53:44 -0300

    fix: change index extension file

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php 
+
+?>
+
+<!DOCTYPE html>
+<head>
+  <meta charset="utf-8">
+  <title> Sistema de Controle de Chaves - UFBA </title>
+</head>
+<html>
+  <header>
+    <h1> SISTEMA DE CONTROLE DE CHAVES</h1>
+  </header>
+  <body></body>
+  <footer></footer>
+</html>
