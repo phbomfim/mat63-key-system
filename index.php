46a4945e303c2963f3046ddabf75e58580346e53
ALCIDES AUGUSTO BEZERRA NETO <alcalcides@hotmail.com>
2022-07-01 22:59:15 -0300

    Adds basic meta tags
    Fixes scope of tag 'body'

--- index.php
+++ index.php
@@ -3,16 +3,18 @@
 ?>
 
 <!DOCTYPE html>
+<html lang="pt-br">
 <head>
-  <meta charset="utf-8">
-  <title> Sistema de Controle de Chaves - UFBA </title>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Sistema de Controle de Chaves - UFBA</title>
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
-  </head>
 </head>
-<html>
+<body>
   <header>
     <h1>SISTEMA DE CONTROLE DE CHAVES</h1>
   </header>
-  <body></body>
   <footer></footer>
+</body>
 </html>
