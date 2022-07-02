99f2f641b59c73823c02c0b151f467e7ad4b4fd3
ALCIDES AUGUSTO BEZERRA NETO <alcalcides@hotmail.com>
2022-07-01 22:33:54 -0300

    Install Bootstrap 5.2.0-beta1

--- index.php
+++ index.php
@@ -6,10 +6,12 @@
 <head>
   <meta charset="utf-8">
   <title> Sistema de Controle de Chaves - UFBA </title>
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
+  </head>
 </head>
 <html>
   <header>
-    <h1> SISTEMA DE CONTROLE DE CHAVES</h1>
+    <h1>SISTEMA DE CONTROLE DE CHAVES</h1>
   </header>
   <body></body>
   <footer></footer>
