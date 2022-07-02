6dbaeefa05aafb93c95f1b6b6ba30528d1d2a6ce
ALCIDES AUGUSTO BEZERRA NETO <alcalcides@hotmail.com>
2022-07-02 18:09:37 -0300

    Show the title 'Salas'

---
+++ view/pages/private/salas.php
@@ -0,0 +1,22 @@
+<?php 
+  $COMPONENTS = "../../components";
+?>
+
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+  <title>Salas | Sistema de Controle de Chaves - UFBA</title>
+  <?php require "$COMPONENTS/meta-general.php" ?>
+</head>
+<body class="container-fluid">
+  <div class="container mt-3 full-screen">
+    <header class="row">
+      <h1 class="col-12 text-center text-strange">Salas</h1>
+    </header>
+    <main class="row my-3">
+      
+    </main>
+  </div>
+  <?php require "$COMPONENTS/footer-general.php" ?>
+</body>
+</html>
