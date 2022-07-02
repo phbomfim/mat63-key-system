fa0c83bc5554c909122a6bea79ccf245ca40bdb7
ALCIDES AUGUSTO BEZERRA NETO <alcalcides@hotmail.com>
2022-07-02 00:26:26 -0300

    Style background and font family

--- index.php
+++ index.php
@@ -10,10 +10,11 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Sistema de Controle de Chaves - UFBA</title>
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
+  <link href="./view/style/theme.css" rel="stylesheet" />
 </head>
-<body>
-  <header>
-    <h1>SISTEMA DE CONTROLE DE CHAVES</h1>
+<body class="container">
+  <header class="row py-1">
+    <h1 class="col-12 text-center">key System</h1>
   </header>
   <footer></footer>
 </body>
