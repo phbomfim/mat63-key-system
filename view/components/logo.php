00ac6a054fc2f866bf578ce19ee1c64bac301abb
ALCIDES AUGUSTO BEZERRA NETO <alcalcides@hotmail.com>
2022-07-02 16:35:36 -0300

    Change paths to absulute way in all components

--- view/components/logo.php
+++ view/components/logo.php
@@ -1,7 +1,3 @@
-<?php
-    $ASSETS = "../assets"
-?>
-
 <div class="w-100 text-center">
-    <img src=<?= "$ASSETS/logo.svg" ?> class="img-fluid rounded-top" alt="Key System Logo">
+    <img src=<?= "/view/assets/logo.svg" ?> class="img-fluid rounded-top" alt="Key System Logo">
 </div>
