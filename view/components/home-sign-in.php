9762fa2cea66a5a36e0d5fe5ef8e4f9e58ca01b1
ALCIDES AUGUSTO BEZERRA NETO <alcalcides@hotmail.com>
2022-07-02 17:15:08 -0300

    Become inputs required

--- view/components/home-sign-in.php
+++ view/components/home-sign-in.php
@@ -1,9 +1,9 @@
-<form id="sign-in">
+<form id="sign-in" action="/controller/sign-in-controller.php" method="post">
     <div class="mb-3">
-        <input type="email" class="form-control" name="email" placeholder="Email">
+        <input type="email" class="form-control" name="email" placeholder="Email" required>
     </div>
     <div class="mb-3">
-        <input type="password" class="form-control" name="password" placeholder="Password">
+        <input type="password" class="form-control" name="password" placeholder="Password" required>
     </div>
     <button type="submit" class="btn btn-danger mb-5">Sign in</button>
 </form>
