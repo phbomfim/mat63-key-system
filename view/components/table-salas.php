8d85d2df603269e645f82eb0562e892b1190408a
Paulo Bomfim <paulobommfim@gmail.com>
2022-07-04 19:15:27 -0300

    Adding more salas

--- view/components/table-salas.php
+++ view/components/table-salas.php
@@ -8,9 +8,34 @@
     </thead>
     <tbody>
         <tr>
-        <td>01</td>
-        <td>50</td>
-        <td>30</td>
+            <td>01</td>
+            <td>50</td>
+            <td>30</td>
+        </tr>
+        <tr>
+            <td>02</td>
+            <td>100</td>
+            <td>60</td>
+        </tr>
+        <tr>
+            <td>03</td>
+            <td>100</td>
+            <td>60</td>
+        </tr>
+        <tr>
+            <td>04</td>
+            <td>45</td>
+            <td>20</td>
+        </tr>
+        <tr>
+            <td>05</td>
+            <td>100</td>
+            <td>60</td>
+        </tr>
+        <tr>
+            <td>06</td>
+            <td>50</td>
+            <td>25</td>
         </tr>
     </tbody>
 </table>
